ff4da88d4f68cbfaee7070884500c45e03bdb869
Victor Costa <victorcosta9743@gmail.com>
2022-10-03 16:39:52 -0300

    api

--- projetoWeb/Program.cs
+++ projetoWeb/Program.cs
@@ -1,15 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/nome", () => new{nome = "Victor", idade="25"});
+app.MapPost("/nome", () => new{nome = "Victor", idade="25"});
 app.MapGet("/rota", (HttpResponse response) => {
     response.Headers.Add("teste", "testeteste");
     return  new{nome = "Victor", idade="25"};
 });
 
 app.MapPost("/saveproduct", (Product product) => {
-    return product.Code + " - " + product.Name;
+    // return product.Code + " - " + product.Name;
+    ProductRepository.Add(product);
+});
+
+app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) => {
+    return dateStart + " - " + dateEnd;
+});
+
+app.MapGet("/getproduct/{code}", ([FromRoute] string code) => {
+    var product = ProductRepository.GetBy(code);
+    return product;
+});
+
+app.MapGet("/getproducts", (HttpRequest request)=>{
+    return request.Headers["product"].ToString();
 });
 
 app.Run();
@@ -17,4 +32,21 @@ app.Run();
 public class Product {
     public string Code { get; set; }
     public string Name { get; set; }
+}
+
+public static class ProductRepository {
+    // criando array
+    public static List<Product> Products {get; set;}
+
+    public static void Add(Product product) {
+        if(Products == null) {
+            Products = new List<Product>();
+        }
+        Products.Add(product);
+
+    }
+
+    public static Product GetBy(string code){
+        return Products.First(p => p.Code == code);
+    }
 }
